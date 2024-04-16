9adc6e597da7b302a3c898ae507c9363ccb34864
Monica Hardt <monicahardt@MacBook-Pro-tilhrende-Monica.local>
2024-04-16 18:48:54 +0200

    fixed script file to run on missing addresses

--- Scripts/copenhagen_zip_retrieval.py
+++ Scripts/copenhagen_zip_retrieval.py
@@ -1,6 +1,12 @@
 import geocoder
 import json
 import re
+from geopy.geocoders import Nominatim
+import csv
+from geopy.location import Location
+
+geolocator = Nominatim(user_agent="my_geocoder")
+
 # go through the json file to locate all addresses with incorrect zipcode
 # then try to retrieve the zipcode again
 f = open('Data/data.json')
@@ -15,7 +21,6 @@ def getZip(info):
     else :
         return
 
-
 addresses_incorrect_location = []
 # we have some json objects where the address exists but the info is null. This happens in 1602 cases (assume this is where the geolocator failed)
 for address, info in data.items():
@@ -28,25 +33,35 @@ for address, info in data.items():
 print(f"The amount of addresses to look up is: {len(addresses_incorrect_location)}")
 
 added_location_to_coordinate = {}
+
 for place in addresses_incorrect_location:
     locations = geocoder.osm(place, maxRows=10)
-    print(locations)
     for location_found in locations:
         zip = location_found.postal
-        print(f"zip found for location: {place} was: {zip}")
+        #print(f"zip found for location: {location_found} was: {zip}")
         if zip is not None:
             try:
                 if 1000 <= int(zip) <= 4999:
-                    added_location_to_coordinate[place] = zip
+                    added_location_to_coordinate[place] = location_found
+                    print(location_found)  
             except:
                 print(f"Failed to convert zip for location {place}, found zip {zip}") 
-    
-print(added_location_to_coordinate)
+                
+# #save as csv file
+# with open('Data/added_location_coord.csv', 'w') as f: 
+#     w = csv.DictWriter(f, added_location_to_coordinate.keys())
+#     w.writeheader()
+#     w.writerow(added_location_to_coordinate)
 
-#save as csv file
-import csv
-with open('Data/added_location_coord.csv', 'w') as f: 
-    w = csv.DictWriter(f, added_location_to_coordinate.keys())
-    w.writeheader()
-    w.writerow(added_location_to_coordinate)
+def location_encoder(obj):
+    return {
+            "latitude"  : obj.lat, 
+            "longitude" : obj.lng,
+            "address"   : obj.address,
+            }
+
+
+#Store the dict as JSON
+with open("added_data.json", "w") as file:
+    json.dump(added_location_to_coordinate, file, default=location_encoder, indent=4)
 
